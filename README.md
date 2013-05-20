24af108ff843cc875ed09cd0a90dbcf25be5cd57
Kyle Robinson Young <kyle@dontkry.com>
2013-05-20 14:31:20 -0700

    copy init template from grunt-init-gruntplugin

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
-generator-gruntplugin
-=====================
+# Grunt Plugin Generator [![Build Status](https://secure.travis-ci.org/yeoman/generator-gruntplugin.png?branch=master)](https://travis-ci.org/yeoman/generator-gruntplugin)
 
-Create a gruntplugin module with yo, including Nodeunit unit tests.
+> Create a Grunt plugin with [Yeoman][], including nodeunit unit tests.
+
+This generator is based of
+[grunt-init-gruntplugin](https://github.com/gruntjs/grunt-init-gruntplugin), authored by the
+magnificient GruntJS team.
+
+Maintained by [Pascal Hartig](https://github.com/passy).
+
+[Yeoman]: http://yeoman.io/
+
+
+## Installation
+
+If you haven't already done so, install [Yeoman][].
+
+Once Yeoman is installed, install this generator via `npm install -g generator-gruntplugin`.
+
+
+## Usage
+
+At the command-line, cd into an empty directory, run this command and follow the prompts.
+
+```
+yo gruntplugin
+```
+
+_Note that this template will generate files in the current directory, so be sure to change to a new directory first if you don't want to overwrite existing files._
+
+
+## License
+
+[MIT License](http://en.wikipedia.org/wiki/MIT_License)
---
+++ app/templates/tasks/name.js
@@ -0,0 +1,50 @@
+/*
+ * <%= props.name %>
+ * <%= props.homepage %>
+ *
+ * Copyright (c) <%= currentYear %> <%= props.author_name %>
+ * Licensed under the <%= props.licenses.join(', ') %> license<%= props.licenses.length === 1 ? '' : 's' %>.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('<%= props.short_name %>', '<%= props.description %>', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(file) {
+      // Concat specified files.
+      var src = file.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join(grunt.util.normalizelf(options.separator));
+
+      // Handle options.
+      src += options.punctuation;
+
+      // Write the destination file.
+      grunt.file.write(file.dest, src);
+
+      // Print a success message.
+      grunt.log.writeln('File "' + file.dest + '" created.');
+    });
+  });
+
+};
